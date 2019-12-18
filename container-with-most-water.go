319210ac864aa9abd9b48008e871ef08b4b57c9e
wus <wus@immatchu.com>
2019-12-18 14:27:32 +0800

    盛最多水的容器

---
+++ container-with-most-water.go
@@ -0,0 +1,55 @@
+/*
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+
+
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+ 
+
+示例:
+
+输入: [1,8,6,2,5,4,8,3,7]
+输出: 49
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/container-with-most-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+package main
+import "fmt"
+func main(){
+    height := []int{1,8,6,2,150,100,8,3,7}
+    fmt.Println(maxArea(height))
+}
+func maxArea(height []int) int {
+    var (
+        l = 0
+        r = len(height) - 1
+        res = 0
+    )
+
+    for l < r {
+        res = max(res,min(height[l], height[r]) * (r - l))
+        if height[l] > height[r] {
+            r--
+        }else{
+            l++
+        }
+    }
+    return res
+}
+func min(a int, b int) int {
+    if a > b {
+        return b
+    }
+    return a
+}
+func max(a int, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
